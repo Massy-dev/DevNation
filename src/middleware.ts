546490034706127964a4b4150f80059e280d415d
Massy-dev <massy1833.dev@gmail.com>
2025-05-06 13:32:51 +0000

    Correction middleware - Version bêta

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,22 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/settings(.*)","/"]);
-
-
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+export default authMiddleware({
+  publicRoutes: [
+    "/",
+    "/api/webhook/clerk",
+    "/api/webhook(.*)",
+    "/api/uploadthing",
+    "/api/trpc(.*)",
+  ],
+  ignoredRoutes: [
+    "/api/webhook/clerk",
+    "/api/webhook(.*)",
+    "/api/uploadthing",
+    "/api/trpc(.*)",
+  ],
+  debug: true
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
