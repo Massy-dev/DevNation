87fac57cb95e5fbfb6cbecf79e76f330643f62f6
Massy-dev <massy1833.dev@gmail.com>
2025-05-06 13:46:15 +0000

    Correction erreur coté client - Version bêta

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,6 +7,9 @@ export default authMiddleware({
     "/api/webhook(.*)",
     "/api/uploadthing",
     "/api/trpc(.*)",
+    "/sign-in",
+    "/sign-up",
+    "/sso-callback"
   ],
   ignoredRoutes: [
     "/api/webhook/clerk",
@@ -14,7 +17,15 @@ export default authMiddleware({
     "/api/uploadthing",
     "/api/trpc(.*)",
   ],
-  debug: true
+  debug: true,
+  afterAuth(auth, req, evt) {
+    // Handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      const signInUrl = new URL('/sign-in', req.url);
+      signInUrl.searchParams.set('redirect_url', req.url);
+      return Response.redirect(signInUrl);
+    }
+  }
 });
 
 export const config = {
